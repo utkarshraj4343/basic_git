9f7d6faa9a6fc99ea432985e24d759992f3dfde8
Utkarsh Raj <utkarshraj4334@gmail.com>
2024-08-01 21:50:24 +0530

    initial commit

---
+++ sum.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int add(int a, int b)
+{
+    return a + b;
+}
+int main()
+{
+    int a = 4;
+    int b = 5;
+    int ans = add(a, b);
+    cout << ans << endl;
+    return 0;
+}
